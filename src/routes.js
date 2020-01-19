1bb9b65c1b04ffc5f1633b7c46698664f1a24527
carloswimmer <coiote777@gmail.com>
2020-01-18 22:40:30 -0300

    feat: Create header styles

--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,27 @@ import Profile from './pages/Profile'
 
 const Routes = createAppContainer(
   createStackNavigator({
-    Main,
-    Profile
+    Main: {
+      screen: Main,
+      navigationOptions: {
+        title: 'DevRadar',
+        headerTitleAlign: "center"
+      }
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        title: 'Github Profile'
+      }
+    }
+  },
+  {
+    defaultNavigationOptions: {
+      headerTintColor: '#fff',
+      headerStyle: {
+        backgroundColor: '#7d40e7'
+      },
+    }
   })
 )
 
