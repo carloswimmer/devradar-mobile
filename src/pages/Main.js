ddff1c1eba3784930eb575f685cdccff8711c0f5
carloswimmer <coiote777@gmail.com>
2020-01-19 00:25:54 -0300

    feat: Show user location on map

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import { StyleSheet, Image, View, Text } from 'react-native'
+import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
 function Main() {
@@ -33,12 +33,52 @@ function Main() {
     return null
   }
 
-  return <MapView initialRegion={currentRegion} style={styles.map} />
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -24.011086, longitude: -46.270919 }}>
+        <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/19734511?s=460&v=4'}} />
+
+        <Callout>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Carlos Wimmer</Text>
+            <Text style={styles.devBio}>FrontEnd Developer focused on efficient interface design and pleasant user experience.</Text>
+            <Text style={styles.devTechs}>ReactJS, Node.js, React Native</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
+  )
 }
 
 const styles = StyleSheet.create({
   map: {
     flex: 1
+  },
+
+  avatar: {
+    height: 54,
+    width: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#fff'
+  },
+
+  callout: {
+    width: 260
+  },
+
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5
+  },
+
+  devTechs: {
+    marginTop: 5
   }
 })
 
