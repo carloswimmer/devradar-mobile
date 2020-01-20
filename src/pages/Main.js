68fc4a84a0c26112e44061e8b3d55c50991a6669
carloswimmer <coiote777@gmail.com>
2020-01-19 21:05:37 -0300

    feat: Receive new devs from websocket

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,7 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import api from '../services/api'
-import { connect, disconnect } from '../services/socket'
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket'
 
 function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null)
@@ -35,7 +35,13 @@ function Main({ navigation }) {
     loadInitialLocation()
   }, [])
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]))
+  }, [devs])
+
   function setupWebsocket() {
+    disconnect()
+
     const { latitude, longitude } = currentRegion
 
     connect(latitude, longitude, techs)
